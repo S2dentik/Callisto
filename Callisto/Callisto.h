bf14867a734f41834bc8f0c89638dd9b650d4834
Alexandru Culeva <alexandru.culeva@ellation.com>
2017-08-15 01:01:19 +0300

    Initial Commit

---
+++ Callisto/Callisto.h
@@ -0,0 +1,19 @@
+//
+//  Callisto.h
+//  Callisto
+//
+//  Created by Alexandru Culeva on 8/15/17.
+//  Copyright © 2017 Ellation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Callisto.
+FOUNDATION_EXPORT double CallistoVersionNumber;
+
+//! Project version string for Callisto.
+FOUNDATION_EXPORT const unsigned char CallistoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Callisto/PublicHeader.h>
+
+
